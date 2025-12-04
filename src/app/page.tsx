e5be74d0576d55ab6e03772876aef74c8f60accd
TeeNzario <teen.2555@gmail.com>
2025-12-04 21:09:45 +0700

    add click on dragon can pop up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [pop, setPop] = useState(false); 
+
+  const hadleClick = () => {
+    console.log('clicked')
+      setPop(true)
+
+    setTimeout(() => {
+      setPop(false)
+    }, 100)
+  }
   return (
     <div className="h-screen bg-[#FFFAEA]">
       <div className="absolute top-5 right-5 flex items-center justify-center bg-[#F5AB3E] border border-[#F5DE93] w-[5rem] h-[2em] rounded-full">
         <p className="text-white font-bold text-end w-full mr-4">999</p>
       </div>
-         <div className="container w-full h-full flex flex-col items-center justify-center">
-          <Image src="/images/main_character.png" alt="dragon_img" width={160} height={160} />
+      <div className="container w-full h-full flex flex-col items-center justify-center">
+          <button onClick={hadleClick}>
+            <Image className={pop ? "scale-110 transition-all duration-50 ease-in-out" : "scale-100 transition-all duration-50 ease-in-out"} src="/images/main_character.png" alt="dragon_img" width={160} height={160} />
+          </button>
           <p className="text-2xl font-bold text-[#B5601D] mt-5">Click on the dragon!</p>
          </div>
    </div>
