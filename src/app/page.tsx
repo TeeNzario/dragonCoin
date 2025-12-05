284fe0d0a76bec4d779e8eff0ba263da98db093e
TeeNzario <teen.2555@gmail.com>
2025-12-05 22:46:32 +0700

    add import useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Textfit } from 'react-textfit'
 
 export default function Home() {
